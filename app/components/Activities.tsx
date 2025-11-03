02898cee2cde9724e20249837a5655420bbec108
nite <gerald.kangombe@posta.co.tz>
2025-11-03 21:49:17 +0300

    front end

---
+++ app/components/Activities.tsx
@@ -0,0 +1,14 @@
+export default function Activities() {
+    return (
+        <div className={"mt-3 rounded-md border-[#6C6C83] border p-3"}>
+            <div className={"flex justify-between"}>
+                <h3>@sulamita_ivy</h3><span>x</span>
+            </div>
+            <h4>5 days ago</h4>
+            <h3>100 Days of code challenge</h3>
+            <div className={"bg-[#2C2D39] py-2 px-2 rounded-md mt-1"}>
+                I&#39;ll have to try this sometime. <br/> Really like this idea. Wanna talk <br/> about it? I&#39;m ...
+            </div>
+        </div>
+    )
+}
